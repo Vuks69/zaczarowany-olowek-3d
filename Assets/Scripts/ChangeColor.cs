063b427946b65aa0f5ef4eae1b5872bc9c10a907
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-07-19 21:17:51 +0200

    MP3-29 menu
    Menu created

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
