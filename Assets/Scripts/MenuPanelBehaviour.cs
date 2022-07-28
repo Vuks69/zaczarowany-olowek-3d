c48423481d4bfe3735fddeebf9555db069c42169
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-07-28 21:50:49 +0200

    MP3-29, MP3-34
    tool parameters menu created, instantiating menu icons initial commit

--- Assets/Scripts/MenuPanelBehaviour.cs
+++ Assets/Scripts/MenuPanelBehaviour.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class MenuPanelBehaviour : MonoBehaviour
 {
     private static Color DEFAULT_COLOR = Color.white;
-	private static Color SELECTED_COLOR = Color.green;
+
+    private static Color SELECTED_COLOR = Color.green;
 
     private Color currentColor = DEFAULT_COLOR;
 
     private bool updateColor = false;
-	private long zmienna = 0;
+
+    private long zmienna = 0;
 
     // Use this for initialization
     void Start()
@@ -20,11 +22,12 @@ public class MenuPanelBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		zmienna++;
-		if(zmienna == 500) {
-			zmienna = 0;
-			ToggleColor();
-		}
+        zmienna++;
+        if (zmienna == 100)
+        {
+            zmienna = 0;
+            ToggleColor();
+        }
         if (updateColor)
         {
             updateColor = false;
---
+++ Assets/Scripts/ToolsMenuBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsMenuBehaviour : MonoBehaviour
+{
+    public GameObject icon;
+
+    public const int ICON_COUNT = 9;
+
+    public List<GameObject> icons = new List<GameObject>();
+
+    public struct RowColumnCount
+    {
+        public int rowCount;
+
+        public int columnCount;
+    }
+
+    void Start()
+    {
+        instantiateIcons();
+    }
+
+    void instantiateIcons()
+    {
+        var rowColumnCount = new RowColumnCount();
+        determineRowAndColumnCount (ref rowColumnCount);
+
+		var x = transform.position.x + 0.1f;
+		var initialY = transform.position.y / 2;
+		var initialZ = transform.position.z / 2;
+
+        for (var i = 0; i < rowColumnCount.rowCount; i++)
+        {
+            for (var j = 0; j < rowColumnCount.columnCount; j++)
+            {
+                var instantiatedIcon =
+                    Instantiate(icon,
+                    new Vector3(x,
+                        initialY,
+                        initialZ),
+                    Quaternion.identity);
+                instantiatedIcon.transform.parent = gameObject.transform;
+                instantiatedIcon.transform.localScale =
+                    new Vector3(0.1f, 0.35f, 0.35f);
+                icons.Add (instantiatedIcon);
+            }
+        }
+    }
+
+    void determineRowAndColumnCount(ref RowColumnCount rowColumnCount)
+    {
+        while (true)
+        {
+            if (
+                rowColumnCount.rowCount * rowColumnCount.columnCount >=
+                ICON_COUNT
+            )
+            {
+                break;
+            }
+            rowColumnCount.rowCount++;
+            if (
+                rowColumnCount.rowCount * rowColumnCount.columnCount >=
+                ICON_COUNT
+            )
+            {
+                break;
+            }
+            rowColumnCount.columnCount++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
