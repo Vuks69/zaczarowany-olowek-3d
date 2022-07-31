f02226102245441e8a38660dfa8c866e27edd668
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-07-31 19:21:52 +0200

    Mateusz features branch (#4)
    * Testowy commit. Dodany napis Hello world.
    
    * Testowy commit. Dodany napis Hello world.
    
    * MP3-29 menu
    
    Menu created
    
    * MP3-29
    
    added method ToggleColor in script MenuPanelBehaviour, which can be called to indicate that the panel is currently selected (controler is pointed towards it)
    
    * MP3-29, MP3-34
    
    tool parameters menu created, instantiating menu icons initial commit
    
    * MP3-29, MP3-34
    
    added instantiating menus icons
    
    * MP3-29, MP3-34
    
    added slider

---
+++ Assets/Scripts/MenuIconBehaviour.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuIconBehaviour : MonoBehaviour
+{
+    private static Color DEFAULT_COLOR = Color.white;
+
+    private static Color SELECTED_COLOR = Color.green;
+
+    protected Color currentColor = DEFAULT_COLOR;
+
+    protected bool updateColor = false;
+
+    protected long zmienna = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        ChangeColor();
+    }
+
+    public void ToggleColor()
+    {
+        updateColor = true;
+        if (currentColor == DEFAULT_COLOR)
+        {
+            currentColor = SELECTED_COLOR;
+            return;
+        }
+        currentColor = DEFAULT_COLOR;
+    }
+
+    public virtual void ChangeColor()
+    {
+        zmienna++;
+        if (zmienna == 100)
+        {
+            zmienna = 0;
+            ToggleColor();
+        }
+        if (updateColor)
+        {
+            updateColor = false;
+            var renderer = gameObject.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", currentColor);
+        }
+    }
+}
