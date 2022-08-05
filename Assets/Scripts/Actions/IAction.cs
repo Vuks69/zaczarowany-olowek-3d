bc55bfe97b1d90a8d144c9151103c099a4a8fc69
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-08-05 21:14:39 +0200

    MP3-78, MP3-3
    added game manager, initial action execution idea

---
+++ Assets/Scripts/Actions/IAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAction
+{
+    public void HandleLeftButton()
+    {
+        // left button is always Undo so no need to override this
+    }
+
+    public virtual void HandleTrigger()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance;
+
+	public Color CurrentColor { get; set; }
+	public GameObject currentParametersMenu;
+	// public GameObject RightFlystick
+	// public GameObject LeftFlystick
+	// public GameObject CurrentSelection
+
+	void Awake() {
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
