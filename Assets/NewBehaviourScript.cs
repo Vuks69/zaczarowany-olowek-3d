8a15a68079c0cb03774e801178907942d8d23ef8
Piotr <63730402+elokhar@users.noreply.github.com>
2022-08-07 22:03:09 +0200

    Added camera and flystick movement using mouse

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float sensX;
+	public float sensY;
+
+	public Transform orientation;
+
+	float xRotation;
+	float yRotation;
+
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;	
+		transform.rotation = Quaternion.Euler(0, 90f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get mouse input
+		float mouseX = Input.GetAxisRaw ("Mouse X") * Time.deltaTime * sensX;
+		float mouseY = Input.GetAxisRaw ("Mouse Y") * Time.deltaTime * sensY;
+
+		yRotation += mouseX;
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp (xRotation, -90f, 90f);
+
+		//rotate cam and orientation
+		transform.rotation = Quaternion.Euler(0, yRotation, xRotation);
+		orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+	}
+}
