5f86794783594dcd503db68071e9a5a9cca55094
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-08-07 21:48:35 +0200

    MP3-3, MP3-78
    Script and game object location refactored, instantiating parameters menu when selected

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
 	public static GameManager Instance;
 
 	public Color CurrentColor { get; set; }
-	public GameObject currentParametersMenu;
+	public GameObject CurrentParametersMenu;
 	// public GameObject RightFlystick
 	// public GameObject LeftFlystick
 	// public GameObject CurrentSelection
@@ -24,4 +24,12 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public void SetCurrentParametersMenu(GameObject parametersMenu)
+    {
+		Instantiate(parametersMenu, CurrentParametersMenu.transform.position, CurrentParametersMenu.transform.rotation);
+		Destroy(CurrentParametersMenu);
+		CurrentParametersMenu = parametersMenu;
+		// update parameters menu
+    }
 }
