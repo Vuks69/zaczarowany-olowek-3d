e7d5ec3b81b3978aa717e66bbe5c5f9b26c488e3
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2022-11-05 23:49:41 +0100

    refactoring

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -34,7 +34,7 @@ namespace Assets.Scripts.Managers
                 int i = 0;
                 foreach (GameObject item in objectList)
                 {
-                    serializableArray.objects[i++] = SerializeObject(item);
+                    serializableArray.objects[i++] = Serializator.SerializeObject(item);
                 }
 
                 // Write new save
@@ -57,57 +57,6 @@ namespace Assets.Scripts.Managers
             //{
             //    Debug.LogError("Savefile not found at " + path);
             //}
-        }
-
-        private SerializableObject SerializeObject(GameObject toSerialize)
-        {
-            SerializableObject serializableObject;
-            switch (toSerialize.name)
-            {
-                case GlobalVars.LineName: // to serialize: transform.position and LineRenderer variables
-                    var lr = toSerialize.GetComponent<LineRenderer>();
-                    serializableObject = new SerializableObject
-                    {
-                        name = toSerialize.name,
-                        tag = toSerialize.tag,
-                        position = new SerializableObject.SerializableVector3
-                        {
-                            x = toSerialize.transform.position.x,
-                            y = toSerialize.transform.position.y,
-                            z = toSerialize.transform.position.z
-                        },
-                        lineRendererData = new SerializableObject.LineRendererData
-                        {
-                            numCapVertices = lr.numCapVertices,
-                            numCornerVertices = lr.numCornerVertices,
-                            positionCount = lr.positionCount,
-                            useWorldSpace = lr.useWorldSpace,
-                            shader = lr.material.shader.name,
-                            startColor = new SerializableObject.SerializableColor
-                            {
-                                r = lr.startColor.r,
-                                g = lr.startColor.g,
-                                b = lr.startColor.b,
-                                a = lr.startColor.a
-                            },
-                            endColor = new SerializableObject.SerializableColor
-                            {
-                                r = lr.endColor.r,
-                                g = lr.endColor.g,
-                                b = lr.endColor.b,
-                                a = lr.endColor.a
-                            },
-                            startWidth = lr.startWidth,
-                            endWidth = lr.endWidth,
-                        }
-                    };
-                    break;
-                default:
-                    Debug.LogError("Attempted serialization of unsupported GameObject [" + toSerialize.name + "]");
-                    return null;
-            }
-
-            return serializableObject;
         }
     }
 }
