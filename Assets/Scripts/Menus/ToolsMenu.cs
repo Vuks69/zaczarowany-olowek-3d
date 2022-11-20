d2959af8736fe1f3d0ffcdf95aa0a9af6ec076a5
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2022-11-20 18:46:58 +0100

    add manipulation to icons

--- Assets/Scripts/Menus/ToolsMenu.cs
+++ Assets/Scripts/Menus/ToolsMenu.cs
@@ -12,10 +12,17 @@ namespace Assets.Scripts.Menus
         public MenuIcon lineDrawingIcon { get; set; } = new ToolsMenuIcon(GameObject.Find("Line Drawing"), GameManager.Instance.ActionsData.LineDrawing, MenuManager.Instance.ParametersMenusData.LineDrawingParametersMenu);
         public MenuIcon erasingIcon { get; set; } = new IconWithoutParametersMenu(GameObject.Find("Erasing"), GameManager.Instance.ActionsData.Erasing);
         public MenuIcon objectSelecting { get; set; } = new ToolsMenuIcon(GameObject.Find("Object Selecting"), GameManager.Instance.ActionsData.ObjectSelecting, MenuManager.Instance.ParametersMenusData.ObjectSelectingParametersMenu);
-
+        public MenuIcon objectManipulation { get; set; } = new IconWithoutParametersMenu(GameObject.Find("Object Manipulation"), GameManager.Instance.ActionsData.Manipulation);
         public ToolsMenu()
         {
-            icons = new List<MenuIcon> { colorPickingIcon, selectingIcon, lineDrawingIcon, erasingIcon, objectSelecting };
+            icons = new List<MenuIcon> {
+                colorPickingIcon,
+                selectingIcon,
+                lineDrawingIcon,
+                erasingIcon,
+                objectSelecting,
+                objectManipulation
+            };
         }
     }
 }
