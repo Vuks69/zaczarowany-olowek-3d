0983ed89ead65947a3c88028a74baa2811afde38
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2022-12-10 16:22:02 +0100

    add skybox selecting

---
+++ Assets/Scripts/Menus/Icons/SkyboxSelectingMenuIcon.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Actions;
+using UnityEngine;
+
+namespace Assets.Scripts.Menus.Icons
+{
+    public class SkyboxSelectingMenuIcon : MenuIcon
+    {
+        private readonly Material skybox;
+        public SkyboxSelectingMenuIcon(GameObject icon, Action action, Material skybox) : base(icon, action)
+        {
+            this.skybox = skybox;
+        }
+
+        public override void Select()
+        {
+            SetSelectedColor();
+            getIconsMenu().SelectedIcon = this;
+            RenderSettings.skybox = skybox;
+        }
+    }
+}
