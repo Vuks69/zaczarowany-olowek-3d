30430409f4188c8b4b5234de8df241532d61e00c
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2023-05-04 19:22:49 +0200

    new way of drawing

--- Assets/Scripts/Serialization/SerializableObjectArrayWrapper.cs
+++ Assets/Scripts/Serialization/SerializableObjectArrayWrapper.cs
@@ -7,13 +7,11 @@ namespace Assets.Scripts.Serialization
     public class SerializableObjectArrayWrapper
     {
         public List<SerializableLine> lines;
-        public List<SerializableLine3D> lines3d;
         public List<SerializablePrimitive> primitives;
 
         public SerializableObjectArrayWrapper()
         {
             this.lines = new List<SerializableLine>();
-            this.lines3d = new List<SerializableLine3D>();
             this.primitives = new List<SerializablePrimitive>();
         }
     }
